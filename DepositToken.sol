2e9d92a13c437491a7a9c5b594f71bbc117c3b22
Roman Potemin <38759021+poteminr@users.noreply.github.com>
2018-07-13 16:11:51 +1000

    Update DepositToken.sol

--- DepositToken.sol
+++ DepositToken.sol
@@ -85,7 +85,7 @@ contract depositToken {
     function ownerOf(uint _tokenId) public view returns (address){
             return depositList[_tokenId].owner;
     }
-    function isContract(address addr) public returns (bool) {
+    function isContract(address addr) public view returns (bool) {
       uint size;
     assembly { size := extcodesize(addr) }
     return size > 0;
