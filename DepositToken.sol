2235cae22a8ec614ab8d51f1666fd74163623bf4
Egor Sechinski <egor@sechinski.ru>
2018-07-15 09:04:51 +1000

    Update DepositToken.sol

--- DepositToken.sol
+++ DepositToken.sol
@@ -12,12 +12,13 @@ contract depositToken {
         uint percent;//процент вклада
         uint startDate;//дата начала вклада
         uint depositTime;//время вклада
-        uint deposiAmount;//начисленная сумма
+        uint depositAmount;//начисленная сумма
         uint percentAmount;//начисленный процент
     }
         uint depositCount=0;//колличество вкладоы
         address dev;//адрес разработчика, для добавления новых банков
         uint[] book;
+        
     mapping (uint => depositStruct) public depositList; //создание словаря где ключ id токена, а значение список параметров
     mapping(address=>bool) isBank;// только банки могут открывать и закрывать вклады
     mapping(address =>uint[]) UserVallet;//кошелек пользователя
@@ -31,7 +32,7 @@ contract depositToken {
         uint id = depositCount;
         isBank[msg.sender] ==true;
         uint timeNow =0;
-        depositList[id] = depositStruct({demolished: false, owner: msg.sender, percent: _percent, startDate:timeNow, depositTime:_depositTime, deposiAmount:_money, percentAmount:0});
+        depositList[id] = depositStruct({demolished: false, owner: msg.sender, percent: _percent, startDate:timeNow, depositTime:_depositTime, depositAmount:_money, percentAmount:0});
         UserVallet[msg.sender].push(id);
         emit CreateDeposit(_percent,timeNow,_depositTime,  msg.sender);
     }
@@ -43,10 +44,11 @@ contract depositToken {
                 emit Transfer(msg.sender, _to, depositList[_tokenId].owner);
             }
     }
-    function transfer(addres _from, addres _to, uint _tokenId) public{
+    function transfer(address _from, address _to, uint _tokenId) public{
      require(depositList[_tokenId].owner == _from||depositList[_tokenId].demolished!=true);
     { //если условие не выполнится будет revert(), т.е откат назад
                 depositList[_tokenId].owner = _to;
+                delTokenVallet(_tokenId);
                 emit Transfer(msg.sender, _to, depositList[_tokenId].owner);
                 //удаление токена из кошелька
             }
@@ -57,17 +59,19 @@ contract depositToken {
             {
                 Config toContract = Config(_to);
                 transfer(_to, _tokenId);
+
                 toContract.onERC721Received(_from,  _to,  _tokenId,  data);
             }
             else{
                 transfer(_from,_to, _tokenId);
-
+                
             }
         }
     
     function demolish(uint _tokenId) public{//закрытие вклада
         require(depositList[_tokenId].owner == msg.sender);//проверка на владельца
         //будет ли токен терять владельца
+        delTokenVallet(_tokenId);
         depositList[_tokenId].demolished=true;
     }
     function registerVendor(address _bank) public
@@ -77,8 +81,8 @@ contract depositToken {
     }
     function getSumm(uint _summ,uint _percent, uint _startDate) public view returns(uint)
     {
-       uint n=(now-_startDate)/2592000;
-       return _summ * (1+(_percent / 12) /100 )**n;
+       uint n=(now-_startDate)/2592000; // изменить
+       return _summ * (1+(_percent / 12) /100 )**n; // изменить 
     }
     function Time_call() public view returns (uint){
         return now;
@@ -99,7 +103,12 @@ contract depositToken {
       uint size;
     assembly { size := extcodesize(addr) }
     return size > 0;
+    }
     
+    function delTokenVallet(uint _tokenId) private
+    {
+        require(depositList[_tokenId].owner == msg.sender);
+        delete UserVallet[msg.sender][getTokenId(_tokenId)];
     }
     function setBank() public //установка пользователя банком 
     {
@@ -108,4 +117,25 @@ contract depositToken {
          isBank[msg.sender]=true;
      }
     }
+    function uintToBytes(uint x) returns (bytes b) {
+    b = new bytes(32);
+    assembly { mstore(add(b, 32), x) }
+}
+    function stringToBytes(string memory source) returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+        return 0x0;
+    }
+
+    assembly {
+        result := mload(add(source, 32))
+    }
+}
+    function getSerializedData(uint _tokenId) public view returns(bytes32[])
+    {
+        bytes32 myData;
+        myData.push(depositStruct.)
+            
+    }
+    
 }
